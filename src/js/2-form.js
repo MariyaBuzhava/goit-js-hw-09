b5b9642d9a9cdea0021db5b101ae0a6e61b0e2ca
Mariya Buzhava <bujava.masha@gmail.com>
2024-07-18 17:35:03 +0300

    update

---
+++ src/js/2-form.js
@@ -0,0 +1,44 @@
+// let formData = {
+//     email: "",
+//     message: ""
+// };
+
+// const form = document.querySelector('.feedback-form');
+// const emailInput = form.querySelector('input[name="email"]');
+// const messageTextarea = form.querySelector('textarea[name="message"]');
+
+// const saveToLocalStorage = () => {
+//     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+// };
+
+// const loadFromLocalStorage = () => {
+//     const savedData = localStorage.getItem('feedback-form-state');
+//     if (savedData) {
+//         formData = JSON.parse(savedData);
+//         emailInput.value = formData.email;
+//         messageTextarea.value = formData.message;
+//     }
+// };
+
+// document.addEventListener('DOMContentLoaded', loadFromLocalStorage);
+
+// form.addEventListener('input', (event) => {
+//     if (event.target.name === 'email') {
+//         formData.email = event.target.value.trim();
+//     } else if (event.target.name === 'message') {
+//         formData.message = event.target.value.trim();
+//     }
+//     saveToLocalStorage();
+// });
+
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     if (!formData.email || !formData.message) {
+//         alert('Fill please all fields');
+//         return;
+//     }
+//     console.log(formData);
+//     localStorage.removeItem('feedback-form-state');
+//     formData = { email: "", message: "" };
+//     form.reset();
+// });
